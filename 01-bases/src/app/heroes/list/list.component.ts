55ca7bc2fb7b00ee66cef992079097f69ae73edf
maría mira <125968901+caindra@users.noreply.github.com>
2025-01-08 11:35:45 +0000

    introducting *ngFor

--- 01-bases/src/app/heroes/list/list.component.ts
+++ 01-bases/src/app/heroes/list/list.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'She-Hulk',
+    'Black Widow',
+    'Captain America',
+    'Black Panther',
+    'Doctor Strange',
+    'Scarlet Witch',
+    'Wolverine',
+    'Deadpool',
+    'Daredevil',
+    'Punisher',
+    'Ghost Rider',
+    'Blade',
+    'Silver Surfer',
+    'Loki',
+    'Iron Fist',
+    'Hawkeye'
+  ];
 }
