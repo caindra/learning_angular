7827434664f77e0ac4d81a79f2812d5172c2f0eb
maría mira <125968901+caindra@users.noreply.github.com>
2025-01-08 10:33:36 +0000

    implemented functionality to hero component

--- 01-bases/src/app/heroes/hero/hero.component.ts
+++ 01-bases/src/app/heroes/hero/hero.component.ts
@@ -11,5 +11,11 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
 
+  getHeroDescription(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
 }
