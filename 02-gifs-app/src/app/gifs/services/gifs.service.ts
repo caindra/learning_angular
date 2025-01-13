6382ae7c2352672cabac28aa993e92faa69c735e
maría mira <125968901+caindra@users.noreply.github.com>
2025-01-13 15:52:07 +0000

    created validation on GifsService

--- 02-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 
+const GIPHY_API_KEY = 'rC3wTuEpqz4jiQfGUXsP0CTwFbbitKP4';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
   private _tagHistory: string[] = [];
+  private apiKey: string = GIPHY_API_KEY;
 
   constructor() { }
 
@@ -13,10 +16,19 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organiseHistory(tag: string){
+    tag = tag.toLowerCase();
+    if(this._tagHistory.includes(tag)){
+      this._tagHistory = this._tagHistory.filter(oldTag => oldTag !== tag);
+    }
     this._tagHistory.unshift(tag);
+    this._tagHistory = this.tagsHistory.splice(0,10);
+  }
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organiseHistory(tag);
     //console.log(this._tagHistory);
   }
 
-  
 }
