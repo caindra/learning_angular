55746a6722aafe063da980cd9ddbd2bc48e6540b
maría mira <125968901+caindra@users.noreply.github.com>
2025-01-13 11:40:02 +0000

    viewchild included

--- 02-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 02-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -10,9 +10,21 @@ import { Component } from '@angular/core';
       type="text"
       class="form-control"
       placeholder="Search a gif..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
     />
   `
 })
 export class SearchBoxComponent {
 
+  // ViewChild: To get a reference to the input element
+  // ElementRef: To get the value of the input element
+  // txtTagInput: To get the value of the input element
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
 }
