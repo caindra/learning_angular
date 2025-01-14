0d8ac54cceb730874d3d43d6b623069eefda1846
maría mira <125968901+caindra@users.noreply.github.com>
2025-01-14 07:36:03 +0000

    made http api request

--- 02-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const GIPHY_API_KEY = 'rC3wTuEpqz4jiQfGUXsP0CTwFbbitKP4';
@@ -9,8 +10,9 @@ export class GifsService {
 
   private _tagHistory: string[] = [];
   private apiKey: string = GIPHY_API_KEY;
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagHistory];
@@ -28,7 +30,37 @@ export class GifsService {
   public searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organiseHistory(tag);
-    //console.log(this._tagHistory);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
+
+    /**
+      // Fetch API
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`)
+          .then(response => response.json())
+          .then(data => console.log(data.data));
+    */
+
+    /**
+      * Using async/await
+      *
+
+        async searchTag(tag: string): Promise<void> {
+          if (tag.length === 0) return;
+          this.organiseHistory(tag);
+
+          const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`);
+          const data = await resp.json();
+          console.log(data);
+        }
+    */
   }
 
 }
