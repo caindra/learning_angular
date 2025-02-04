0035c5a8b3dabe538292f30d623d0041ac1d10d3
caindra <mmirgal868@g.educaand.es>
2025-02-04 13:58:07 +0100

    cleaned subscribes + optimised region selection

--- 03-country-app/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 03-country-app/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,20 +2,31 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
-public countries: Country[] = [];
+  public countries: Country[] = [];
+  public isLoading: boolean = false;
+
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService){}
 
-  searchByRegion(term: string): void{
-    this.countriesService.searchRegion(term)
+  searchByRegion(region: Region): void{
+    this.isLoading = true;
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion(region)
     .subscribe( countries => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
