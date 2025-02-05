6eb912cbbc63cba11778f26ce82307ca821e6f24
caindra <maria31mx@gmail.com>
2025-02-05 15:32:07 +0100

    created missing app module

---
+++ 04-pipes-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers:[],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
